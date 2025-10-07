ff2abded452b21f5da8e833b48fab4d21b6eb890
baielldev <bajel8844@gmail.com>
2025-10-07 19:00:43 +0600

    .

--- src/api/register/index.ts
+++ src/api/register/index.ts
@@ -2,9 +2,16 @@
 import { useMutation } from "@tanstack/react-query";
 import axios from "axios";
 
+type RegisterUser = {
+  avatar: string;
+  userName: string;
+  email: string;
+  password: string;
+};
+
 const usePostUserMutation = () => {
   return useMutation({
-    mutationFn: async (newUser) => {
+    mutationFn: async (newUser: RegisterUser) => {
       const response = await axios.post(
         `http://localhost:5000/api/auth/register`,
         newUser
